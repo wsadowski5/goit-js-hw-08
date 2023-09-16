2ceedf05e99b6db70dd7634b2dcac227566faa3a
wsadowski5 <130093777+wsadowski5@users.noreply.github.com>
2023-09-16 13:00:11 +0200

    commit hw8
    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,4 +4,39 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form')
 const localStorageKey = 'feedback-form-state'
 
-console.log(form)
+
+const input = form.querySelector('input')
+const textarea = form.querySelector('textarea')
+const button = form.querySelector('button')
+
+const values = {
+    email: '',
+    message: '',
+}
+
+
+const inputHandler = throttle((event) => {
+    values.message = textarea.value;
+    values.email = input.value;
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+}, 500)
+
+form.addEventListener('input', inputHandler)
+
+const parsedValues = JSON.parse(localStorage.getItem(localStorageKey));
+
+form.elements.message.value = parsedValues.message; 
+form.elements.email.value = parsedValues.email;
+
+
+const buttonHandler = (event) => {
+    event.preventDefault();
+    console.log(textarea.value, input.value)
+    localStorage.clear();
+    form.reset()
+}
+
+button.addEventListener('click', buttonHandler)
+
+
+
