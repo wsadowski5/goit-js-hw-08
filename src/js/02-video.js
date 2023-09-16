00aaacfcfdc0eb9d75a2b7d7de4ee3263061182a
wsadowski5 <130093777+wsadowski5@users.noreply.github.com>
2023-09-16 09:21:18 +0200

    commit hw8
    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time'
+
+player.on('timeupdate', throttle((event) => {
+    const currentTime = event.seconds;
+    localStorage.setItem(localStorageKey, currentTime);
+}, 1000)
+)
+
+const getCurrentTime = localStorage.getItem(localStorageKey)
+
+console.log(getCurrentTime)
+
+player.setCurrentTime(getCurrentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
 
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form')
+const localStorageKey = 'feedback-form-state'
+
+console.log(form)
