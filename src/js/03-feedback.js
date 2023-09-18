4567f27cd8bd4b47a400cb2641551a895ba5fa77
wsadowski5 <130093777+wsadowski5@users.noreply.github.com>
2023-09-18 10:44:19 +0200

    commit hw 8
    poprawki

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,36 +7,47 @@ const localStorageKey = 'feedback-form-state'
 
 const input = form.querySelector('input')
 const textarea = form.querySelector('textarea')
-const button = form.querySelector('button')
 
 const values = {
     email: '',
     message: '',
 }
 
-
 const inputHandler = throttle((event) => {
-    values.message = textarea.value;
     values.email = input.value;
+    values.message = textarea.value;
     localStorage.setItem(localStorageKey, JSON.stringify(values));
 }, 500)
 
 form.addEventListener('input', inputHandler)
 
-const parsedValues = JSON.parse(localStorage.getItem(localStorageKey));
 
-form.elements.message.value = parsedValues.message; 
-form.elements.email.value = parsedValues.email;
-
-
-const buttonHandler = (event) => {
-    event.preventDefault();
-    console.log(textarea.value, input.value)
-    localStorage.clear();
-    form.reset()
+const onLoad = () => {
+    const valuesToParse = localStorage.getItem(localStorageKey);
+    if (!valuesToParse) {
+        form.elements.message.value = ""
+    form.elements.email.value = ""
+    }
+    else {
+        const parsedValues = JSON.parse(valuesToParse);
+        form.elements.message.value = parsedValues.message 
+        form.elements.email.value = parsedValues.email 
+    }
 }
+onLoad();
 
-button.addEventListener('click', buttonHandler)
+const formHandler = (event) => {
+    event.preventDefault();
+    if ((form.elements.message.value && form.elements.email.value) === "") {
+        alert ('Please fill all fields!!')
+    }
+    else {
+    console.log(values);
+    localStorage.clear();
+    form.reset();
+}
+}
+form.addEventListener('submit', formHandler)
 
 
 
